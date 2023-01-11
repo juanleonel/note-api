4264835ee7f2f95c0aaae8d5a208ccbc1dbe265e
Juan Albores <jlac_1993@outlook.com>
2023-01-11 17:22:02 -0500

    added auth to endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,10 @@
-import App from "./app";
-const portNumber = process.env.PORT ? Number(process.env.PORT) : 8080;
+import App from './app';
+import { CONFIGURATIONS } from './config/configurations';
+
+const portNumber =
+  typeof CONFIGURATIONS.PORT_DEFUALT === 'string'
+  ? parseInt(CONFIGURATIONS.PORT_DEFUALT)
+  : CONFIGURATIONS.PORT_DEFUALT;
 
 App.app.listen(portNumber, 'localhost', () => {
   console.log('Listen on localhost:' + portNumber);
